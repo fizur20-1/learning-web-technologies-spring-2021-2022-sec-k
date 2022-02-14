e5cd4a639487c73de50f1d6adb04cbe9542d5b01
fizur20-1 <91717919+fizur20-1@users.noreply.github.com>
2022-02-14 13:50:41 +0600

    lab 5
    new file added

---
+++ MID_LAB_TASK_05_PHP_FORM/taskB.php
@@ -0,0 +1,44 @@
+<?php 
+
+	
+$username= " ";
+	
+		if($_REQUEST['username'] == null){
+			$unameError = "username is empty....";
+		}else{
+			$username = $_REQUEST['username'];
+			echo $username;
+		}
+	
+?>
+
+
+
+<html>
+<head>
+	<title>name</title>
+</head>
+<body>
+	<fieldset>
+		<legend>NAME</legend>
+	<form method="POST" action="">
+		<table>
+			<tr>
+				
+				<td><input type="text" name="username" value=""></td>
+				<td> 
+					
+				</td>
+			</tr>
+			
+			<tr>
+			      <td><hr> </hr></td>
+			 </tr>
+			 <tr>  
+				<td><input type="submit" name="submit" value="Submit"></td>
+			</tr>
+		</table>
+	</fieldset>
+	</form>
+</body>
+</html>
