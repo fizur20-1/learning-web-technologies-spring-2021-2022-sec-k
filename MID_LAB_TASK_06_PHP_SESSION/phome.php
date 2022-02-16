3d3f01ad8619f0bda12cb1a5cc812b4b0bbe0c10
fizur20-1 <91717919+fizur20-1@users.noreply.github.com>
2022-02-16 11:49:55 +0600

    LAB 6
    NEW FILE ADDED

---
+++ MID_LAB_TASK_06_PHP_SESSION/phome.php
@@ -0,0 +1,12 @@
+<html>
+<head>	
+	<title>Home Page</title>
+</head>
+<body>
+	<h1>Welcome to xCompany </h1> 
+</br>
+	<a href="phome.php"> Home </a>
+	<a href="plogin.php"> Login </a>
+	<a href="preg.php"> Registration </a>
+</body>
+</html>
